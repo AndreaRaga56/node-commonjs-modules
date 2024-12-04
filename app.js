75cf09dafff287c4e92e9c251ea5d7f677010658
AndreaRaga56 <andrearaga56@gmail.com>
2024-12-04 18:49:34 +0100

    feat fine

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const hobbies = require ('./hobbies.js');
 const names = require ('./names.js');
-console.log(hobbies.creaHobby)
 
 const persona = ()=>{
-    hobbies.creaHobby(a, b, c);
-    names.fullName (d, f);    
+    const oggetto = {
+        ...names.fullName ('Andrea', 'Ragazzoli'),
+        ...hobbies.creaHobby('Cinema','Musica','Letteratura')        
+    }
+    return oggetto;
 };
+
+console.log(persona());
 
